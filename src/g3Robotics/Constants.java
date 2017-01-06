3c2ecd0fde9e51afdb97081b890fbaf9309406bf
pratikkunapuli <pratikkunapuli@host-68-234-129-175.gaatcmb.clients.pavlovmedia.com>
2017-01-06 15:00:14 -0500

    Commiting from Pratik's branch to test functionality.

--- src/g3Robotics/Constants.java
+++ src/g3Robotics/Constants.java
@@ -7,7 +7,7 @@ package g3Robotics;
 public class Constants {
 
 	//PWM Outputs
-	public static final int leftDrivePWM1 = 3;
+	public static final int leftDrivePWM1 = 2;
 	public static final int rightDrivePWM1 = 10;
 	
 	//Solenoid Outputs
