8260da7bd011553a040be8ea92c33a056538c2c2
gruffen <lexi431@gmail.com>
2017-02-03 17:07:47 -0500

    Tuned vision processing pipeline, incorporates axis camera

--- src/g3Robotics/vision/GripPipeline.java
+++ src/g3Robotics/vision/GripPipeline.java
@@ -28,7 +28,8 @@ import org.opencv.objdetect.*;
 public class GripPipeline implements VisionPipeline {
 
 	//Outputs
-	private Mat hslThresholdOutput = new Mat();
+	private Mat cvCvtcolorOutput = new Mat();
+	private Mat cvThresholdOutput = new Mat();
 	private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
 	private ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
 
@@ -40,21 +41,26 @@ public class GripPipeline implements VisionPipeline {
 	 * This is the primary method that runs the entire pipeline and updates the outputs.
 	 */
 	@Override	public void process(Mat source0) {
-		//Filter through HSL threshold:
-		Mat hslThresholdInput = source0;
-		double[] hslThresholdHue = {69.60431654676259, 126.96969696969698};
-		double[] hslThresholdSaturation = {137.58992805755395, 255.0};
-		double[] hslThresholdLuminance = {59.62230215827338, 255.0};
-		hslThreshold(hslThresholdInput, hslThresholdHue, hslThresholdSaturation, hslThresholdLuminance, hslThresholdOutput);
+		// Step CV_cvtColor0:
+		Mat cvCvtcolorSrc = source0;
+		int cvCvtcolorCode = Imgproc.COLOR_BGR2GRAY;
+		cvCvtcolor(cvCvtcolorSrc, cvCvtcolorCode, cvCvtcolorOutput);
 
-		//Find the contours:
-		Mat findContoursInput = hslThresholdOutput;
+		// Step CV_Threshold0:
+		Mat cvThresholdSrc = cvCvtcolorOutput;
+		double cvThresholdThresh = 190.0;
+		double cvThresholdMaxval = 255.0;
+		int cvThresholdType = Imgproc.THRESH_BINARY;
+		cvThreshold(cvThresholdSrc, cvThresholdThresh, cvThresholdMaxval, cvThresholdType, cvThresholdOutput);
+
+		// Step Find_Contours0:
+		Mat findContoursInput = cvThresholdOutput;
 		boolean findContoursExternalOnly = false;
 		findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
 
-		//Filter contours:
+		// Step Filter_Contours0:
 		ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
-		double filterContoursMinArea = 124.0;
+		double filterContoursMinArea = 50.0;
 		double filterContoursMinPerimeter = 0;
 		double filterContoursMinWidth = 0;
 		double filterContoursMaxWidth = 1000;
@@ -65,32 +71,73 @@ public class GripPipeline implements VisionPipeline {
 		double filterContoursMinVertices = 0;
 		double filterContoursMinRatio = 0;
 		double filterContoursMaxRatio = 1000;
-		filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, 
-				filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, 
-				filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, 
-				filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, 
-				filterContoursOutput);
+		filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
+
 	}
 
-	public Mat hslThresholdOutput() {
-		return hslThresholdOutput;
+	/**
+	 * This method is a generated getter for the output of a CV_cvtColor.
+	 * @return Mat output from CV_cvtColor.
+	 */
+	public Mat cvCvtcolorOutput() {
+		return cvCvtcolorOutput;
 	}
 
+	/**
+	 * This method is a generated getter for the output of a CV_Threshold.
+	 * @return Mat output from CV_Threshold.
+	 */
+	public Mat cvThresholdOutput() {
+		return cvThresholdOutput;
+	}
+
+	/**
+	 * This method is a generated getter for the output of a Find_Contours.
+	 * @return ArrayList<MatOfPoint> output from Find_Contours.
+	 */
 	public ArrayList<MatOfPoint> findContoursOutput() {
 		return findContoursOutput;
 	}
 
+	/**
+	 * This method is a generated getter for the output of a Filter_Contours.
+	 * @return ArrayList<MatOfPoint> output from Filter_Contours.
+	 */
 	public ArrayList<MatOfPoint> filterContoursOutput() {
 		return filterContoursOutput;
 	}
 
-	private void hslThreshold(Mat input, double[] hue, double[] sat, double[] lum,
-		Mat out) {
-		Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HLS);
-		Core.inRange(out, new Scalar(hue[0], lum[0], sat[0]),
-			new Scalar(hue[1], lum[1], sat[1]), out);
+
+	/**
+	 * Converts an image from one color space to another.
+	 * @param src Image to convert.
+	 * @param code conversion code.
+	 * @param dst converted Image.
+	 */
+	private void cvCvtcolor(Mat src, int code, Mat dst) {
+		Imgproc.cvtColor(src, dst, code);
 	}
 
+	/**
+	 * Apply a fixed-level threshold to each array element in an image.
+	 * @param src Image to threshold.
+	 * @param threshold threshold value.
+	 * @param maxVal Maximum value for THRES_BINARY and THRES_BINARY_INV
+	 * @param type Type of threshold to appy.
+	 * @param dst output Image.
+	 */
+	private void cvThreshold(Mat src, double threshold, double maxVal, int type,
+		Mat dst) {
+		Imgproc.threshold(src, dst, threshold, maxVal, type);
+	}
+
+	/**
+	 * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
+	 * @param input The image on which to perform the Distance Transform.
+	 * @param type The Transform.
+	 * @param maskSize the size of the mask.
+	 * @param output The image in which to store the output.
+	 */
 	private void findContours(Mat input, boolean externalOnly,
 		List<MatOfPoint> contours) {
 		Mat hierarchy = new Mat();
@@ -106,6 +153,23 @@ public class GripPipeline implements VisionPipeline {
 		Imgproc.findContours(input, contours, hierarchy, mode, method);
 	}
 
+
+	/**
+	 * Filters out contours that do not meet certain criteria.
+	 * @param inputContours is the input list of contours
+	 * @param output is the the output list of contours
+	 * @param minArea is the minimum area of a contour that will be kept
+	 * @param minPerimeter is the minimum perimeter of a contour that will be kept
+	 * @param minWidth minimum width of a contour
+	 * @param maxWidth maximum width
+	 * @param minHeight minimum height
+	 * @param maxHeight maximimum height
+	 * @param Solidity the minimum and maximum solidity of a contour
+	 * @param minVertexCount minimum vertex Count of the contours
+	 * @param maxVertexCount maximum vertex Count
+	 * @param minRatio minimum ratio of width to height
+	 * @param maxRatio maximum ratio of width to height
+	 */
 	private void filterContours(List<MatOfPoint> inputContours, double minArea,
 		double minPerimeter, double minWidth, double maxWidth, double minHeight, double
 		maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double
@@ -137,6 +201,9 @@ public class GripPipeline implements VisionPipeline {
 			output.add(contour);
 		}
 	}
+
+
+
 
 }
 
